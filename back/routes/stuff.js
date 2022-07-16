04f94b581d1bfbf45ba3454855ddcb42e6d698f7
LHCRYPT <l.hamadouche@laposte.net>
2022-07-16 16:33:43 +0200

    installation de multer

--- back/routes/stuff.js
+++ back/routes/stuff.js
@@ -4,9 +4,9 @@ const router = express.Router(); //on créé le routeur avec la méthode routeur
 const stuffCtrl = require('../controllers/stuff'); //on importe stuff de controllers avec stuffCtrl
 
 
-router.get('/', auth, stuffCtrl.getAllThings);//pour récupérer toutes les choses
+router.get('/', auth, stuffCtrl.getAllThings);//pour récupérer toutes les choses; on met auth avant le gestionnaire de route afin qu'il soiy utilisé en premier sinon pas d'authentification
 router.post('/', auth, multer, stuffCtrl.createThing); //Dans notre routeur stuff, nous importons notre middleware et le passons comme argument aux routes à protéger
-router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing); 
 router.put('/:id', auth, multer, stuffCtrl.modifyThing); //on met multer pour pouvoir modifier des objets
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
