3d75d5ab2770ff4861b367bee77c877b2d0f510e
LHCRYPT <l.hamadouche@laposte.net>
2022-07-18 17:55:25 +0200

    j'ai enlevé les node modules

--- back/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express') //commande pour importer le package/ pour importer le contenu d'un module JavaScript on utilise require
-const app = express()
-const port = 3000
-
