d040941197be3b8f7fd3df28a6da7596c60ea482
LHCRYPT <l.hamadouche@laposte.net>
2022-07-27 17:21:35 +0200

    ajout heat

--- back/models/Thing.js
+++ back/models/Thing.js
@@ -4,6 +4,7 @@ const thingSchema = mongoose.Schema({ //on crée un schéma de données avec tou
     title: { type: String, required: true }, //on crée un objet pour configurer le titre, c'est un champs obligatoire(=required: true)
     description: { type: String, required: true },
     imageUrl: { type: String, required: true }, //Pas besoin de mettre un champ pour l'Id puisqu'il est automatiquement généré par Mongoose
+    heat: { type: String, required: true },
     userId: { type: String, required: true },
     price: { type: Number, required: true },
     likes: { type: Number},
